f289d890cbaa119da8563a86b207ac0135c86554
Sarah Wester <softlytothedawn@gmail.com>
2020-03-09 12:21:10 +0800

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
+
+  # http://localhost:3000/ask -> controller: question  action: ask
+  # prot -  server      - route             controller       : action
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
